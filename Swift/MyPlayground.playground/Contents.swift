1ff36452b29ab404ef38391c4a2a8fa028e0af08
Young Park <i.make.seyoung@gmail.com>
2016-05-31 15:49:18 +0300

    Swift

--- Swift/MyPlayground.playground/Contents.swift
+++ Swift/MyPlayground.playground/Contents.swift
@@ -6,9 +6,18 @@ import UIKit
 func logIfNeeded(log: AnyObject, doWeWantToPrint: Bool=false) {
     if doWeWantToPrint { print("\(log)") }
 }
-func logIfNeeded(log: Bool, doWeWantToPrint: Bool=false) {
-    if doWeWantToPrint { print("\(log)") }
-}
+
 print(true)
 logIfNeeded("sdf")
 logIfNeeded("asdf", doWeWantToPrint: true)
+
+extension Double {
+//    func isNeg()-> Bool = self < 0
+    var isNeg: Bool{ return self < 0 }
+}
+
+var d: Double = -3
+if d.isNeg{
+    d = Double.abs(d)
+}
+print(d)
